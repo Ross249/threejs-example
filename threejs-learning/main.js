730b64c61112cc9ca9be2c9ad789fdd141197900
Ross <528266131@qq.com>
2022-08-19 23:41:19 +0800

    try gui

--- threejs-learning/main.js
+++ threejs-learning/main.js
@@ -1,5 +1,41 @@
 import * as THREE from "three";
 
+import * as dat from "dat.gui";
+
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10,
+  },
+};
+
+function generationPlane() {
+  plane.geometry.dispose();
+  plane.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+
+  const { array } = plane.geometry.attributes.position;
+
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    array[i + 2] = z + Math.random();
+  }
+}
+gui.add(world.plane, "width", 1, 20).onChange(generationPlane);
+gui.add(world.plane, "height", 1, 20).onChange(generationPlane);
+gui.add(world.plane, "widthSegments", 1, 20).onChange(generationPlane);
+gui.add(world.plane, "heightSegments", 1, 20).onChange(generationPlane);
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -11,9 +47,41 @@ const camera = new THREE.PerspectiveCamera(
 
 const renderer = new THREE.WebGLRenderer();
 
-console.log(scene);
-console.log(camera);
-console.log(renderer);
-
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+
 document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide,
+  flatShading: true,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+
+const { array } = plane.geometry.attributes.position;
+
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+
+  array[i + 2] = z + Math.random();
+}
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
+
+animate();
