3750a3c63573aeb8cc187aeb967dab9520ac3fe8
Ross <528266131@qq.com>
2022-09-03 15:20:58 +0800

    init globe project

---
+++ globe/main.js
@@ -0,0 +1,22 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerHeight / innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
